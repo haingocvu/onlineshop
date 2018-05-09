98dedcf1223dae3d0c76837e66e872799a4a85d4
vnhai <haingocvu@hotmail.com>
2018-05-09 17:15:04 +0700

    show menubar with data gotten from database

--- controller/BaseController.php
+++ controller/BaseController.php
@@ -1,8 +1,12 @@
 <?php  
 
+require_once "model/BaseModel.php";
+
 class BaseController
 {
 	protected function loadView($view = "home", $data = []){
+		$baseModel = new BaseModel();
+		$menus = $baseModel->getMenu();
 		include_once "view/layout.view.php";
 	}
 }
---
+++ model/BaseModel.php
@@ -0,0 +1,18 @@
+<?php  
+
+require_once "DBConnect.php";
+
+class BaseModel extends DBConnect
+{
+	public function getMenu(){
+		$sql = "SELECT menu.name, menu.icon, menu.url, GROUP_CONCAT(sub.name, ':', sub.url) as submenu
+					FROM (SELECT categories.*, page_url.url FROM categories INNER JOIN page_url ON categories.id_url = page_url.id WHERE id_parent IS NULL) as menu
+					LEFT JOIN (SELECT categories.*, page_url.url FROM categories INNER JOIN page_url ON page_url.id = categories.id_url WHERE id_parent IS NOT NULL) as sub
+					ON menu.id = sub.id_parent
+					GROUP BY menu.id
+					ORDER BY submenu DESC";
+		return $this->loadMoreRow($sql);
+	}	
+}
+
+?>
