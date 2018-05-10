86a0e2aaf79fb639eaa9a9cca6bb4ece8ef1fffd
vnhai <haingocvu@hotmail.com>
2018-05-10 18:02:50 +0700

    show products by types

--- controller/TypeController.php
+++ controller/TypeController.php
@@ -1,11 +1,29 @@
 <?php  
 
 require_once "BaseController.php";
+require_once "model/TypeModel.php";
 
 class TypeController extends BaseController
 {
 	function getProductByType(){
-		return $this->loadView("type");
+		$data = [];
+		//get alias from url, if empty, set default iphone-x
+		$alias = isset($_GET["alias"])?$_GET["alias"]:"iphone-x";
+		$typeModel = new TypeModel();
+		$catgory = $typeModel->getCategory($alias);
+		//check if user enter invalid alias
+		if($catgory != '' && $catgory != false){
+			if($catgory->id_parent != null){
+				$productsByType = $typeModel->getProductsByTypeLevel2($alias);
+			}else {
+				$productsByType = $typeModel->getProductsByTypeLevel1($alias);
+			}
+			$data = [
+			"productsByType"=>$productsByType,
+			"catgory"=> $catgory
+			];
+		}
+		return $this->loadView("type", $data);
 	}
 }
 
---
+++ model/TypeModel.php
@@ -0,0 +1,42 @@
+<?php  
+
+require_once "DBConnect.php";
+
+class TypeModel extends DBConnect{
+
+	public function getCategory($alias){
+		$sql = "SELECT categories.*, page_url.url 
+				FROM categories INNER JOIN page_url on page_url.id = categories.id_url
+				WHERE page_url.url = '$alias'";
+		return $this->loadOneRow($sql);
+	}
+
+	public function getProductsByTypeLevel2($alias){
+		$sql = "SELECT prod.*, url.url FROM (SELECT products.*
+				FROM (SELECT categories.id
+				FROM categories INNER JOIN page_url ON categories.id_url = page_url.id
+				WHERE page_url.url = '$alias') AS cat INNER JOIN products ON products.id_type = cat.id) as prod INNER JOIN page_url as url
+				ON prod.id_url = url.id";
+		return $this->loadMoreRow($sql);
+	}
+
+	public function getProductsByTypeLevel1($alias){
+		$sql = "SELECT products.*, page_url.url
+				FROM (SELECT parent.*
+				FROM (SELECT categories.* FROM categories INNER JOIN page_url on page_url.id = categories.id_url
+				WHERE page_url.url = '$alias') as parent
+				LEFT JOIN (SELECT categories.* FROM categories WHERE categories.id_parent IS NOT NULL) as child
+				ON parent.id = child.id_parent
+				UNION
+				SELECT child.*
+				FROM (SELECT categories.* FROM categories INNER JOIN page_url on page_url.id = categories.id_url
+				WHERE page_url.url = '$alias') as parent
+				INNER JOIN (SELECT categories.* FROM categories WHERE categories.id_parent IS NOT NULL) as child
+				ON parent.id = child.id_parent) as cat
+				INNER JOIN products ON cat.id = products.id_type
+				INNER JOIN page_url ON page_url.id = products.id_url";
+		return $this->loadMoreRow($sql);
+	}
+}
+
+?>
